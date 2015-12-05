db8cb3182ff1415dc75292cc5776ce596bc9b423
pthaike <pthaike@gmail.com>
2015-12-06 00:57:16 +0800

    merge project

---
+++ js/ruzhuren.js
@@ -0,0 +1,10 @@
+function close3() {
+	document.getElementById('center_zhu_2').style.display='none';
+	document.getElementById('center_zhu_3').style.display='block';
+}
+
+function tianjiaruzhuren(){
+	document.getElementById('center_zhu_3').style.display='none';
+	document.getElementById('center_zhu_2').style.display='block';
+}
+
